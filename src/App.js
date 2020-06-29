99259a6c0e0639468b7622cdfb327c291b532848
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-06-29 13:58:05 -0400

    completed workshop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Ticker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+      pause: false,
+      interval: 0,
+      pausebtn: "Pause"
+    };
+  };
 
-export default App;
+  componentDidMount() {
+
+    this.setState({
+      interval: setInterval(() => {
+          this.setState({
+            count: this.state.count + 1
+          })
+        }, 1000)
+      })
+    
+
+  }
+ 
+  shouldComponentUpdate(nextProps, nextState) {
+
+    // if(nextState.count % 3 === 0){
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    if(!this.state.pause){
+      return true;
+    } else {
+      return false;
+    } 
+
+
+  }
+
+  resetTicker = () => {
+    this.setState({
+      count: 0,
+    })
+  }
+
+  pauseTicker = () => {
+    this.setState({
+      pause: !(this.state.pause)
+    })
+    if(!this.state.pause){
+      this.setState({
+        pausebtn: "Play",
+        interval: clearInterval(this.state.interval),
+      })
+    } else {
+      this.setState({
+        pausebtn: "Pause",
+        interval: setInterval(() => {
+            this.setState({
+              count: this.state.count + 1
+            })
+          }, 1000),
+        })
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <p>The Ticker is: {this.state.count} </p>
+        <button type = "button" onClick = {this.resetTicker}>Reset Ticker</button>
+        <button type = "button" onClick = {this.pauseTicker}>{this.state.pausebtn}</button>
+      </div>
+    );
+  };
+
+
+};
+
+export default Ticker;
